1ae75d1ebce7772c1fc14a7690c1193a9008162a
Vince Cabonita <vinceryancabonita@gmail.com>
2025-02-09 00:09:02 +0800

    Implement getPopularMovies service to fetch popular movies from TMDB API p2

--- lib/app/data/services/movie_service.dart
+++ lib/app/data/services/movie_service.dart
@@ -1,14 +1,59 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:movie_browser/app/domain/entities/movie.dart';
+import 'package:movie_browser/app/domain/exceptions/impl_exception.dart';
+import 'package:movie_browser/app/domain/exceptions/invalid_api_response_exception.dart';
+import 'package:movie_browser/app/domain/exceptions/network_exception.dart';
+import 'package:movie_browser/app/domain/exceptions/service_exception.dart';
+import 'package:movie_browser/utils/env_config.dart';
 
 class MovieService {
   final http.Client httpClient;
 
   MovieService({required this.httpClient});
 
-  Future<List<Movie>> getMovies() async {
-    // TODO: implement getMovie
-    throw UnimplementedError();
-  }
+  Future<List<Movie>> getPopularMovies(
+      {String language = 'en-US', int page = 1}) async {
+    final Uri uri = Uri.parse('${EnvConfig.apiBaseUrl}/movie/popular').replace(
+      queryParameters: {
+        'language': language,
+        'page': page.toString(),
+      },
+    );
 
+    final headers = {
+      'Authorization': 'Bearer ${EnvConfig.bearerToken}',
+      'accept': 'application/json',
+    };
+
+    try {
+      final response = await http.get(uri, headers: headers);
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+
+        if (jsonData is Map<String, dynamic> &&
+            jsonData.containsKey('results')) {
+          final results = jsonData['results'] as List<dynamic>;
+
+          final movies =
+              results.map((movieJson) => Movie.fromJson(movieJson)).toList();
+          return movies;
+        } else {
+          throw InvalidApiResponseException('Invalid API response format');
+        }
+      } else {
+        throw ServiceException(
+          'Error fetching movies: ${response.reasonPhrase}',
+          statusCode: response.statusCode,
+        );
+      }
+    } on SocketException {
+      throw NetworkException('No internet connection');
+    } catch (e) {
+      throw ImplException('An unexpected error occurred: $e');
+    }
+  }
 }
